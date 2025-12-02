579f44c755ed78b9e6343a371344a23a64d247f0
20ali4051 <aliyigitdemir43@gmail.com>
2025-12-02 09:14:42 +0300

    Add files via upload

---
+++ büyük olanı bul/büyük olanı bul/Buyukolanıbul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace büyük_olanı_bul
+{
+    internal class Buyukolanıbul
+    {
+        public int Bul(int a, int b)
+        {
+            int sonuc;
+            if (a < b)
+            {
+                return sonuc = b;
+            }
+            else
+            {
+                return sonuc = a;
+            }
+        }
+    }
+}
